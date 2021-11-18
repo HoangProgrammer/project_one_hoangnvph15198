bbc1a43f95424e84d486a29c62103968498f73f1
quan <quynhhoa688622@gmail.com>
2021-11-18 20:06:07 +0700

    quan2

--- layout/layout_1/footer.php
+++ layout/layout_1/footer.php
@@ -1,6 +1,29 @@
 
     <!-- Required Js -->
-    <footer-main class="viewport__footer" ng-if="LayoutMain.layout.footer">
+
+
+    <!-- <footer style="height: 60px;" class="pcoded-main-container pcoded-main-footer" >
+        <div class="course-lesson__footer">
+            <div class="course-lesson__footer-left">
+                <span class="course-lesson__footer-item">2021 © Busuu Ltd</span>
+                <a class="course-lesson__footer-item" href="">Điều khoản</a>
+                <a class="course-lesson__footer-item" href="">Bảo mật</a>
+            </div>
+            <div class="course-lesson__footer-right">
+                <span class="course-lesson__footer-heft">Trợ giúp</span>
+                <i class="fab fa-facebook course-lesson__footer-heft"></i>
+                <i class="fab fa-internet-explorer course-lesson__footer-heft"></i>
+                <i class="fab fa-instagram course-lesson__footer-heft"></i>
+                <i class="fab fa-youtube course-lesson__footer-heft"></i>
+            </div>
+        </div>
+    </footer> -->
+
+
+    <!-- footer của Hoàng -->
+
+
+    <footer-main class=" viewport__footer" ng-if="LayoutMain.layout.footer">
         <footer class="footer-main">
             <ul class="footer-main__link-list">
             <li class="footer-main__copyright footer-main__link-item footer-main__link-item--no-spacing">
@@ -14,6 +37,9 @@
                 <a class="footer-main__link" href="https://help.busuu.com/hc/vi" target="_blank" rel="noreferrer noopener">Trợ giúp</a>
             </li>
         </ul>
+
+
+
         <!-- <div class="footer-main__socials">
 
                 <a class="footer-main__socials__links icon" target="_blank" href="https://twitter.com/busuu" rel="noreferrer noopener">
