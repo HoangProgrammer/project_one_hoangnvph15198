eea93c21a98d5543c9db368aba26de5cee2d103d
thanh <thanhntph15251@fpt.edu.vn>
2021-11-19 21:59:48 +0700

    update home2

--- site/home2.php
+++ site/home2.php
@@ -24,7 +24,7 @@
                                 <!--[ daily sales section ] start-->
                                 <a href="site/hoc/learn.php" class="col-md-6 col-xl-4">
                                     <div class="card daily-sales course-english">
-                                        <img class="course-english-img" src="assets/images/slider/giaotiep.png" alt="">
+                                        <img class="course-english-img" src="assets/images/slider/english-communication.jpg" alt="">
                                         
                                     </div>
                                     <span class="course-english-tile" >
@@ -33,7 +33,7 @@
                                 </a>
                                 <a href="" class="col-md-6 col-xl-4">
                                     <div class="card daily-sales course-english">
-                                        <img class="course-english-img" src="assets/images/slider/giaotiep.png" alt="">
+                                        <img class="course-english-img" src="assets/images/slider/english-communication2.jpg" alt="">
                                     </div>
                                     <span class="course-english-tile">
                                         HTML, CSS từ Zero đến Hero
@@ -41,7 +41,7 @@
                                 </a>
                                 <a href="" class="col-md-6 col-xl-4">
                                     <div class="card daily-sales course-english">
-                                        <img class="course-english-img" src="assets/images/slider/giaotiep.png" alt="">
+                                        <img class="course-english-img" src="assets/images/slider/english-communication3.jpg" alt="">
                                     </div>
                                     <span class="course-english-tile" >
                                         HTML, CSS từ Zero đến Hero
@@ -60,7 +60,7 @@
             </div>
 
             <div class="pcoded-content">
-                <h3 class="pcoded-content-name">Khóa chưa học</h3>
+                <h3 class="pcoded-content-name">Có thể bạn thích</h3>
                 <div class="pcoded-inner-content">
                     <!-- [ breadcrumb ] start -->
 
@@ -107,7 +107,7 @@
             </div>
 
             <div class="pcoded-content">
-                <h3 class="pcoded-content-name">Khóa chưa học</h3>
+                <h3 class="pcoded-content-name">Khóa học nổi bật</h3>
                 <div class="pcoded-inner-content">
                     <!-- [ breadcrumb ] start -->
 
