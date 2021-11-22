2f0cde4c5bd2f1a2960f17ce1aa897e67eb0f7a2
hoang <nguyenvanhoang2444@gmail.com>
2021-11-22 15:39:46 +0700

    day lai

--- site/home2.php
+++ site/home2.php
@@ -11,13 +11,21 @@
     </div>
 
       <?php
-                             $id_user;
-                           $Get_oderCourse=Get_oderCourse();
+    //   $error=false;
+    //                          $id_user;
+    //                        $Get_oderCourse=Get_oderCourse();
                            
-                             foreach ($Get_oderCourse as $value){   
-                                 $id_users=$value['id_user'];                        
-                          } 
-                                if(  $id_users !=$id_user){
+    //                          foreach ($Get_oderCourse as $value){   
+                               
+    //                              if(  $id_users ==$id_user){
+    //                               echo   $error=true;
+    //                              } else{
+    //                                 echo    $error =false;
+    //                              }                     
+    //                       } 
+
+
+                                if( !isset($_SESSION['name_user'])){
 
                              }else{ ?>
 
@@ -62,72 +70,15 @@
             <?php  }    ?>
 
 
-   
-            <div class="pcoded-main-container">
-        <div class="pcoded-wrapper">
-
-            <div class="pcoded-content">
-                <h3 class="pcoded-content-name">Khóa chưa học</h3>
-                <div class="pcoded-inner-content">
-               
-                    <div class="main-body">
-                        <div class="page-wrapper">
-                          
-                            <div class="row">
-
-                            
-                             <?php    
-
-                                   $id_user;
-                                   $Get_oderCourse=Get_oderCourse(); 
-                                     foreach ($Get_oderCourse as $value):
-                                     if( $value['id_user']==$id_user):      
-                             $course= Get_caurse();                            
-                             foreach ($course as $key=> $values):
-                            if(  $values['id_caurse']== $value['id_caurse']  && $values['type']==1){
-                                 $other_id= $values['id_caurse'];
- 
-                                
-                                    }    
-                                  endforeach;    
-endif; endforeach;
-
-$conn=connect();
-    $stmt=$conn->prepare("SELECT * FROM course where id_caurse <> $other_id ");
-    $stmt->execute();
-     $stmt->rowCount();
-           $row=$stmt->fetchAll(\PDO::FETCH_ASSOC);
-          
-                                foreach (  $row as $other){ ?>                                                                   
-                                     <a href="index.php?act=detail_course&id_course=<?=$other['id_caurse'] ?>" class="col-md-6 col-xl-4">
-                                    <div class="card daily-sales course-english">
-                                        <img class="course-english-img" src="image/<?=$other['img'] ?>" alt="">                                     
-                                    </div>
-                                    <span class="course-english-tile" >
-                                        <?=$other['NameCaurse'] ?>
-                                    </span>
-                                </a>      
-
-                                <?php } ?>
-
-                                     
-
-                            </div>
-                        </div>
-                    </div>                  
-                </div>
-            </div>
-            </div>
-                
-            </div>
 
 
-<!-- 
+
+
     <div class="pcoded-main-container">
         <div class="pcoded-wrapper">
 
             <div class="pcoded-content">
-                <h3 class="pcoded-content-name">Khóa chưa học</h3>
+                <h3 class="pcoded-content-name">Khóa c</h3>
                 <div class="pcoded-inner-content">
                
                     <div class="main-body">
@@ -157,4 +108,4 @@ $conn=connect();
             </div>
             </div>
                 
-            </div> -->
+            </div>
